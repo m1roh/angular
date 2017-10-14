e9f05c3c73615b19bdd4c71c0dc0c6984685adf2
m1roh <hayromain.rh@gmail.com>
2017-10-14 14:26:25 +0200

    Better solution: using the controller to store users in an array, and adding style

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,6 +1,14 @@
 // MAIN CONTROLLER
 function mainController() {
-  //let vm = this;
+  let vm = this;
+  let users = [];
 
-  //vm.text = vm.firstName;
+  vm.add = function() {
+    users.push(new Array(vm.firstName, vm.lastName, vm.email));
+    vm.firstName = null;
+    vm.lastName = null;
+    vm.email = null;
+  }
+
+  vm.users = users;
 }
